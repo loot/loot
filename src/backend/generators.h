9dbdf7504474d65cb8cc5a394f71ad0da4b9587b
WrinklyNinja <wrinklyninja1@gmail.com>
2014-02-11 17:52:00 +0000

    Corrected untranslatable string.

--- src/backend/generators.h
+++ src/backend/generators.h
@@ -261,7 +261,7 @@ namespace boss {
             note.append_attribute("id").set_value("noChanges");
             note.text().set(boost::locale::translate("No change in details since last run.").str().c_str());
             */
-            messages.push_front(boss::Message(boss::g_message_say, "There have been no changes in the Details tab since BOSS was last run for this game."));
+            messages.push_front(boss::Message(boss::g_message_say, boost::locale::translate("There have been no changes in the Details tab since BOSS was last run for this game.").str()));
         }
 
         if (!messages.empty()) {
