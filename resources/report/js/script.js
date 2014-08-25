36e21ad9fa77ed0e5048e5dfedb2f26165641806
WrinklyNinja <wrinklyninja1@gmail.com>
2014-08-25 18:15:36 +0100

    Removed dead function.

--- resources/report/js/script.js
+++ resources/report/js/script.js
@@ -407,9 +407,6 @@ function hideElement(element) {
         element.classList.toggle('hidden', true);
     }
 }
-function highlightElement(element, state) {
-    element.classList.toggle('highlight', state);
-}
 function toggleDisplayCSS(evt) {
     var e = document.getElementsByClassName(evt.target.getAttribute('data-class'));
     if (evt.target.checked) {
