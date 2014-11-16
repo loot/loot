95628df7adfd8c7a563741c4dbfa594bf0f88117
Oliver Hamlet <oliver.hamlet@gmail.com>
2014-11-16 10:45:17 +0000

    Fixed translation script.
    This also fixes #339, which just goes to show why #338 is important.

--- resources/report/js/l10n.js
+++ resources/report/js/l10n.js
@@ -189,7 +189,7 @@
             document.getElementById('hideBashTags').label = l10n.translate("Hide Bash Tags").fetch();
             document.getElementById('hideNotes').label = l10n.translate("Hide notes").fetch();
             document.getElementById('hideDoNotCleanMessages').label = l10n.translate("Hide 'Do not clean' messages").fetch();
-            document.getElementById('hideInactivePluginMessages').label = l10n.translate("Hide inactive plugin messages").fetch();
+            document.getElementById('hideInactivePlugins').label = l10n.translate("Hide inactive plugin messages").fetch();
             document.getElementById('hideAllPluginMessages').label = l10n.translate("Hide all plugin messages").fetch();
             document.getElementById('hideMessagelessPlugins').label = l10n.translate("Hide messageless plugins").fetch();
             document.getElementById('hiddenPluginsTxt').textContent = l10n.translate("Hidden plugins:").fetch();
