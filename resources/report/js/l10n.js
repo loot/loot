c0402846bd2c7ed686e7c92bf79e7d1556c2e33b
Oliver Hamlet <oliver.hamlet@gmail.com>
2015-01-06 09:17:59 +0000

    Don't bundle Jed Gettext Parser dependency.
    I've now registered it on Bower, so it can be managed like the other JS
    library dependencies.

--- resources/report/js/l10n.js
+++ resources/report/js/l10n.js
@@ -3,7 +3,7 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['bower_components/Jed/jed', 'js/jedGettextParser'], factory);
+        define(['bower_components/Jed/jed', 'bower_components/jed-gettext-parser/jedGettextParser'], factory);
     } else {
         // Browser globals
         root.amdWeb = factory(root.jed, root.jedGettextParser);
