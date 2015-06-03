9ea373b27f188bac10c252080be9d8ea45c0219a
Oliver Hamlet <oliver.hamlet@gmail.com>
2015-06-03 10:21:54 +0100

    Remove ellipses from content filter label.

--- resources/report/js/l10n.js
+++ resources/report/js/l10n.js
@@ -246,7 +246,7 @@
             document.getElementById('sidebarTabs').firstElementChild.textContent = l10n.translate("Plugins").fetch();
             document.getElementById('sidebarTabs').firstElementChild.nextElementSibling.textContent = l10n.translate("Filters").fetch();
             document.getElementById('contentFilter').parentElement.label = l10n.translate("Press Enter or click outside the input to set the filter.").fetch();
-            document.getElementById('contentFilter').label = l10n.translate("Filter content...").fetch();
+            document.getElementById('contentFilter').label = l10n.translate("Filter content").fetch();
 
             /* Filters */
             document.getElementById('hideVersionNumbers').label = l10n.translate("Hide version numbers").fetch();
