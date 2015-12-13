e31c97b3e9a40eb50fdaeb6fe16ce000c0d002dd
Oliver Hamlet <oliver.hamlet@gmail.com>
2015-12-13 13:12:17 +0000

    Run JavaScript tests on Travis
    Using Sauce Labs and Grunt. Also install runtime dependencies
    using Bower to get the Jed package that's used in testing, and
    save the Bower version resolutions for Travis so that step can be
    automated.

---
+++ gruntfile.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = (grunt) => {
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          base: '',
+          port: 9999,
+        },
+      },
+    },
+    'saucelabs-mocha': {
+      all: {
+        options: {
+          urls: ['http://127.0.0.1:9999/src/tests/gui/html/js/test.html'],
+          build: process.env.TRAVIS_JOB_ID,
+          throttled: 3,
+          browsers: [{
+            browserName: 'chrome',
+            platform: 'Windows 10',
+            version: '47',
+          }],
+          testname: 'LOOT UI JS tests',
+        },
+      },
+    },
+    watch: {},
+  });
+
+  // Loading dependencies
+  for (const key in grunt.file.readJSON('package.json').devDependencies) {
+    if (key !== 'grunt' && key.indexOf('grunt') === 0) {
+      grunt.loadNpmTasks(key);
+    }
+  }
+
+  grunt.registerTask('dev', ['connect', 'watch']);
+  grunt.registerTask('test', ['connect', 'saucelabs-mocha']);
+};
