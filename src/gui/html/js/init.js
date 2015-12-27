1f3784c27986d0f2e1d52912b1ef5a660f6205ad
Oliver Hamlet <oliver.hamlet@gmail.com>
2015-12-27 12:35:53 +0000

    Move setupEventHandlers

--- src/gui/html/js/init.js
+++ src/gui/html/js/init.js
@@ -26,11 +26,76 @@ function applyEnabledFilters() {
   if (!loot.filters) {
     return;
   }
+  function setupEventHandlers() {
+    /* Set up handlers for filters. */
+    document.getElementById('hideVersionNumbers').addEventListener('change', onToggleDisplayCSS);
+    document.getElementById('hideVersionNumbers').addEventListener('change', saveFilterState);
+    document.getElementById('hideCRCs').addEventListener('change', onToggleDisplayCSS);
+    document.getElementById('hideCRCs').addEventListener('change', saveFilterState);
+    document.getElementById('hideBashTags').addEventListener('change', onToggleBashTags);
+    document.getElementById('hideBashTags').addEventListener('change', saveFilterState);
+    document.getElementById('hideNotes').addEventListener('change', onSidebarFilterToggle);
+    document.getElementById('hideDoNotCleanMessages').addEventListener('change', onSidebarFilterToggle);
+    document.getElementById('hideInactivePlugins').addEventListener('change', onSidebarFilterToggle);
+    document.getElementById('hideAllPluginMessages').addEventListener('change', onSidebarFilterToggle);
+    document.getElementById('hideMessagelessPlugins').addEventListener('change', onSidebarFilterToggle);
+    document.body.addEventListener('loot-filter-conflicts', onConflictsFilter);
 
   if (loot.settings.filters) {
     for (const filter in loot.settings.filters) {
       loot.filters[filter] = loot.settings.filters[filter];
       document.getElementById(filter).checked = loot.filters[filter];
+    /* Set up event handlers for content filter. */
+    document.getElementById('contentFilter').addEventListener('change', onSidebarFilterToggle);
+
+    /* Set up handlers for buttons. */
+    document.getElementById('redatePluginsButton').addEventListener('click', onRedatePlugins);
+    document.getElementById('openLogButton').addEventListener('click', onOpenLogLocation);
+    document.getElementById('wipeUserlistButton').addEventListener('click', onClearAllMetadata);
+    document.getElementById('copyLoadOrderButton').addEventListener('click', onCopyLoadOrder);
+    document.getElementById('copyContentButton').addEventListener('click', onCopyContent);
+    document.getElementById('refreshContentButton').addEventListener('click', onContentRefresh);
+    document.getElementById('settingsButton').addEventListener('click', onShowSettingsDialog);
+    document.getElementById('helpButton').addEventListener('click', onOpenReadme);
+    document.getElementById('aboutButton').addEventListener('click', onShowAboutDialog);
+    document.getElementById('quitButton').addEventListener('click', onQuit);
+    document.getElementById('updateMasterlistButton').addEventListener('click', onUpdateMasterlist);
+    document.getElementById('sortButton').addEventListener('click', onSortPlugins);
+    document.getElementById('applySortButton').addEventListener('click', onApplySort);
+    document.getElementById('cancelSortButton').addEventListener('click', onCancelSort);
+    document.getElementById('sidebarTabs').addEventListener('core-select', onSwitchSidebarTab);
+    document.getElementById('jumpToGeneralInfo').addEventListener('click', onJumpToGeneralInfo);
+
+    /* Set up search event handlers. */
+    document.getElementById('showSearch').addEventListener('click', onSearchOpen);
+    document.getElementById('searchBar').addEventListener('loot-search-close', onSearchClose);
+    window.addEventListener('keyup', onFocusSearch);
+
+    /* Set up event handlers for settings dialog. */
+    const settings = document.getElementById('settingsDialog');
+    settings.getElementsByClassName('accept')[0].addEventListener('click', onCloseSettingsDialog);
+    settings.getElementsByClassName('cancel')[0].addEventListener('click', onCloseSettingsDialog);
+
+    /* Set up handler for opening and closing editors. */
+    document.body.addEventListener('loot-editor-open', onEditorOpen);
+    document.body.addEventListener('loot-editor-close', onEditorClose);
+    document.body.addEventListener('loot-copy-metadata', onCopyMetadata);
+    document.body.addEventListener('loot-clear-metadata', onClearMetadata);
+
+    document.getElementById('cardsNav').addEventListener('click', onSidebarClick);
+    document.getElementById('cardsNav').addEventListener('dblclick', onSidebarClick);
+
+    /* Set up handler for plugin message and dirty info changes. */
+    document.addEventListener('loot-plugin-message-change', onPluginMessageChange);
+    document.addEventListener('loot-plugin-isdirty-change', onPluginIsDirtyChange);
+
+    /* Set up event handlers for game member variable changes. */
+    document.addEventListener('loot-game-folder-change', onGameFolderChange);
+    document.addEventListener('loot-game-masterlist-change', onGameMasterlistChange);
+    document.addEventListener('loot-game-global-messages-change', onGameGlobalMessagesChange);
+    document.addEventListener('loot-game-plugins-change', onGamePluginsChange);
+  }
+
     }
   }
 
