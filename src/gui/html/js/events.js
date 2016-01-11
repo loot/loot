loot/loot

commit 47c8c0715d6415d2dc49b09b32015f82fc641bb5

Don't ask to apply an unchanged load order
Instead, notify that no changes were made. Closes #523.

src/gui/html/js/events.js
@@ -129,6 +129,18 @@ function onSortPlugins() {
     if (!result) {
       return;
     }
+    /* Check if sorted load order differs from current load order. */
+    const loadOrderIsUnchanged = result.every((plugin, index) => {
+      return plugin.name === loot.game.plugins[index].name;
+    });
+    if (loadOrderIsUnchanged) {
+      /* Send cancelSort query to notify that no unapplied sorting changes are
+         present. Not doing so prevents LOOT's window from closing. */
+      loot.query('cancelSort');
+      loot.Dialog.closeProgress();
+      loot.Dialog.showNotification(loot.l10n.translate('Sorting made no changes to the load order.'));
+      return;
+    }
     loot.game.oldLoadOrder = loot.game.plugins;
     loot.game.loadOrder = [];
     result.forEach((plugin) => {

commit 12a5f44af4d70404e738bb31f0082344a04e74b2

Fix CRCs not displayed after sorting
This is a regression introduced in
47c8c0715d6415d2dc49b09b32015f82fc641bb5.

src/gui/html/js/events.js
@@ -134,6 +134,15 @@ function onSortPlugins() {
       return plugin.name === loot.game.plugins[index].name;
     });
     if (loadOrderIsUnchanged) {
+      result.forEach((plugin) => {
+        const existingPlugin = loot.game.plugins.find((item) => {
+          return item.name === plugin.name;
+        });
+        if (existingPlugin) {
+          existingPlugin.crc = plugin.crc;
+          existingPlugin.isEmpty = plugin.isEmpty;
+        }
+      });
       /* Send cancelSort query to notify that no unapplied sorting changes are
          present. Not doing so prevents LOOT's window from closing. */
       loot.query('cancelSort');