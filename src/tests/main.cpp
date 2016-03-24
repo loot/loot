06d7a4b31bd3804a4cafc2f395f72d19895167e8
Oliver Hamlet <oliver.hamlet@gmail.com>
2016-03-24 19:05:21 +0000

    Improve File tests

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -52,7 +52,7 @@
 #include "backend/helpers/yaml_set_helpers_test.h"
 #include "backend/metadata/condition_grammar_test.h"
 #include "backend/metadata/conditional_metadata_test.h"
-#include "backend/metadata/test_file.h"
+#include "backend/metadata/file_test.h"
 #include "backend/metadata/test_location.h"
 #include "backend/metadata/test_message.h"
 #include "backend/metadata/test_message_content.h"
