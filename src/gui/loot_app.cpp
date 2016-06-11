353435a9e45d24eda15401c8f1aaccda53daeeb7
Oliver Hamlet <oliver.hamlet@gmail.com>
2016-06-11 12:31:42 +0100

    Disable extensions to avoid logging loading error
    LOOT doesn't bundle the extensions resources, because it doesn't use
    extensions, but this has resulted in errors being logged that aren't
    actually a problem.

--- src/gui/loot_app.cpp
+++ src/gui/loot_app.cpp
@@ -58,6 +58,7 @@ namespace loot {
 
             // Disable spell checking.
             command_line->AppendSwitch("--disable-spell-checking");
+            command_line->AppendSwitch("--disable-extensions");
         }
     }
 
