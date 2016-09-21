9af29b99b736b8bc6efcd150077fe917a784d89b
Oliver Hamlet <oliver.hamlet@gmail.com>
2016-09-21 19:40:20 +0100

    Remove link to old metadata syntax doc

--- src/gui/query/get_game_data_query.h
+++ src/gui/query/get_game_data_query.h
@@ -99,7 +99,7 @@ private:
           "menu to open its data folder, then open your 'userlist.yaml' "
           "file in the relevant game folder. You can then edit the "
           "metadata it contains with reference to the "
-          "[syntax documentation](https://loot.github.io/docs/%2%.%3%.%4%/LOOT%%20Metadata%%20Syntax.html).\n\n"
+          "documentation, which is accessible through LOOT's main menu.\n\n"
           "You can also seek support on LOOT's forum thread, which is "
           "linked to on [LOOT's website](https://loot.github.io/)."
         )) % e.what() % LootVersion::major % LootVersion::minor % LootVersion::patch).str()));
