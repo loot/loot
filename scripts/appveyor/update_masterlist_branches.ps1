41c713e3dca886ad92b488c43d2e3f030b256198
Oliver Hamlet <oliver.hamlet@gmail.com>
2016-11-04 19:17:21 +0000

    Automatically set default masterlist branches on release

---
+++ scripts/appveyor/update_masterlist_branches.ps1
@@ -0,0 +1,23 @@
+function Get-BranchName() {
+  $numbers = $env:APPVEYOR_REPO_TAG_NAME.Split('.')
+
+  if ($numbers[0] -eq 0) {
+    $numbers = $numbers[0..($numbers.length - 2)]
+  } else {
+    $numbers = @($numbers[0])
+  }
+
+  return "v" + ($numbers -join '.')
+}
+
+if ($env:APPVEYOR_REPO_TAG -eq 'true') {
+  npm install -g lomad
+  $branchName = Get-BranchName
+
+  Write-Output "`nUpdating masterlists' default branch to $branchName..."
+  lomad -t $env:access_token -a -d $branchName
+
+  if ($lastexitcode -eq 0) {
+    Write-Output "Masterlists' default branches have been updated."
+  }
+}
