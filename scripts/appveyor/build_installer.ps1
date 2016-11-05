fb34466db4a7a0e43f80756373fd85da0fc68236
Oliver Hamlet <oliver.hamlet@gmail.com>
2016-11-05 10:01:47 +0000

    Always build installer on AppVeyor
    So that issues are discovered more quickly. Don't cache the Inno Setup
    directory to keep things simple.

--- scripts/appveyor/build_installer.ps1
+++ scripts/appveyor/build_installer.ps1
@@ -7,22 +7,16 @@ function DownloadLanguageFile($languageFile, $innoPath) {
 
 $innoInstallPath = 'C:\Program Files (x86)\Inno Setup 5'
 
-if ($env:APPVEYOR_REPO_TAG -eq 'true') {
-  if (!(Test-Path $innoInstallPath)) {
-    choco install -y InnoSetup
+# Install the Inno Download Plugin
+$url = 'https://bitbucket.org/mitrich_k/inno-download-plugin/downloads/idpsetup-1.5.1.exe'
+(New-Object System.Net.WebClient).DownloadFile($url, (pwd).path + '/idpsetup-1.5.1.exe')
+./idpsetup-1.5.1.exe /verysilent
 
-    # Install the Inno Download Plugin
-    $url = 'https://bitbucket.org/mitrich_k/inno-download-plugin/downloads/idpsetup-1.5.1.exe'
-    (New-Object System.Net.WebClient).DownloadFile($url, (pwd).path + '/idpsetup-1.5.1.exe')
-    ./idpsetup-1.5.1.exe /verysilent
+# Also install the unofficial Korean and Simplified Chinese translation
+# files for Inno Setup.
+DownloadLanguageFile '\Korean.isl'  $innoInstallPath
+DownloadLanguageFile '\ChineseSimplified.isl'  $innoInstallPath
 
-    # Also install the unofficial Korean and Simplified Chinese translation
-    # files for Inno Setup.
-    DownloadLanguageFile '\Korean.isl'  $innoInstallPath
-    DownloadLanguageFile '\ChineseSimplified.isl'  $innoInstallPath
-  }
+$env:PATH += ';' + $innoInstallPath
 
-  $env:PATH += ';' + $innoInstallPath
-
-  iscc scripts\installer.iss
-}
+iscc scripts\installer.iss
