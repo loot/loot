d810f25bc65a292ca0a7f48c6ab2f03f3da4465a
Oliver Hamlet <oliver.hamlet@gmail.com>
2016-11-05 09:10:44 +0000

    Fix building installer on AppVeyor
    Download and install the Inno Download Plugin.

--- scripts/appveyor/build_installer.ps1
+++ scripts/appveyor/build_installer.ps1
@@ -1,3 +1,9 @@
+function DownloadLanguageFile($languageFile, $innoPath) {
+  $url = 'https://raw.github.com/jrsoftware/issrc/master/Files/Languages/Unofficial/' + $languageFile
+  $installPath = $innoPath + '\Languages\' + $languageFile
+
+  (New-Object System.Net.WebClient).DownloadFile($url, $installPath)
+}
 
 $innoInstallPath = 'C:\Program Files (x86)\Inno Setup 5'
 
@@ -5,14 +11,15 @@ if ($env:APPVEYOR_REPO_TAG -eq 'true') {
   if (!(Test-Path $innoInstallPath)) {
     choco install -y InnoSetup
 
+    # Install the Inno Download Plugin
+    $url = 'https://bitbucket.org/mitrich_k/inno-download-plugin/downloads/idpsetup-1.5.1.exe'
+    (New-Object System.Net.WebClient).DownloadFile($url, (pwd).path + '/idpsetup-1.5.1.exe')
+    ./idpsetup-1.5.1.exe /verysilent
+
     # Also install the unofficial Korean and Simplified Chinese translation
     # files for Inno Setup.
-    $languagesPath = $innoInstallPath + '\Languages'
-    $koreanUrl = 'https://raw.github.com/jrsoftware/issrc/master/Files/Languages/Unofficial/Korean.isl'
-    $chineseUrl = 'https://raw.github.com/jrsoftware/issrc/master/Files/Languages/Unofficial/ChineseSimplified.isl'
-
-    (New-Object System.Net.WebClient).DownloadFile($koreanUrl, $languagesPath + '\Korean.isl')
-    (New-Object System.Net.WebClient).DownloadFile($chineseUrl, $languagesPath + '\ChineseSimplified.isl')
+    DownloadLanguageFile '\Korean.isl'  $innoInstallPath
+    DownloadLanguageFile '\ChineseSimplified.isl'  $innoInstallPath
   }
 
   $env:PATH += ';' + $innoInstallPath
