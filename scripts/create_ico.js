c80d1eb5f7acd712d9a64999ca6b4bd963a6f37c
Oliver Hamlet <oliver.hamlet@gmail.com>
2016-11-29 19:17:15 +0000

    Remove ICO file from repository
    Instead generate it as a pre-build step. It can't be generated as part
    of the CMake build process because MSVC changes the PATH so that another
    convert.exe conflicts with imagemagick's.

--- scripts/create_ico.js
+++ scripts/create_ico.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const svgPath = path.join('resources', 'icon.svg');
 const buildDirectory = path.join('build', 'icon');
-const outputPath = path.join('resources', 'icon.ico');
+const outputPath = path.join(buildDirectory, 'icon.ico');
 
 const sizes = [16, 20, 24, 30, 32, 36, 40, 48, 60, 64, 72, 80, 96, 128, 256];
 
@@ -29,5 +29,3 @@ sizes.forEach((size) => {
 
 convertArgs.push(outputPath);
 childProcess.execFileSync('convert', convertArgs);
-
-fs.removeSync(buildDirectory);
