5a1ccb159d0b0c58b71c06a8bbb744740e283d8c
Oliver Hamlet <oliver.hamlet@gmail.com>
2017-01-06 09:36:58 +0000

    Remove Inkscape dependency for ICO creation
    Just use Imagemagick. Fixes #743.

--- scripts/create_ico.js
+++ scripts/create_ico.js
@@ -6,6 +6,9 @@ const svgPath = path.join('resources', 'icon.svg');
 const buildDirectory = path.join('build', 'icon');
 const outputPath = path.join(buildDirectory, 'icon.ico');
 
+const svgSize = 192;
+const svgDPI = 90;
+
 const sizes = [16, 20, 24, 30, 32, 36, 40, 48, 60, 64, 72, 80, 96, 128, 256];
 
 fs.mkdirsSync(buildDirectory);
@@ -15,15 +18,13 @@ sizes.forEach((size) => {
   const pngFile = path.join(buildDirectory, `icon-${size}.png`);
   convertArgs.push(pngFile);
 
-  helpers.safeExecFileSync('inkscape', [
-    '-z',
-    '-e',
-    pngFile,
-    '-w',
-    size,
-    '-h',
-    size,
+  helpers.safeExecFileSync('convert', [
+    '-density',
+    (size / svgSize) * svgDPI,
+    '-background',
+    'none',
     svgPath,
+    pngFile,
   ]);
 });
 
