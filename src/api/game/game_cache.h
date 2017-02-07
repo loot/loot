b05baad2dd8b8ddadb33f60586fcbf8fcdcb747a
Oliver Hamlet <oliver.hamlet@gmail.com>
2017-02-07 18:26:36 +0000

    Fix compiler performance warning

--- src/api/game/game_cache.h
+++ src/api/game/game_cache.h
@@ -67,8 +67,8 @@ private:
 namespace std {
 template<>
 struct less<std::shared_ptr<const loot::Plugin>> {
-  size_t operator() (const std::shared_ptr<const loot::Plugin>& lhs,
-                     const std::shared_ptr<const loot::Plugin>& rhs) const {
+  bool operator() (const std::shared_ptr<const loot::Plugin>& lhs,
+                   const std::shared_ptr<const loot::Plugin>& rhs) const {
     return lhs->GetLowercasedName() < rhs->GetLowercasedName();
   }
 };
