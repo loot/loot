0cc02d1494120ede6de5b589b82fefe0738533d8
Oliver Hamlet <oliver.hamlet@gmail.com>
2017-12-10 11:53:42 +0000

    Make installer build failures fail the overall build
    $LastExitCode is blank after running idpsetup-1.5.1.exe for some
    reason, so hopefully $? will be sufficient.

--- scripts/appveyor/build_installer.ps1
+++ scripts/appveyor/build_installer.ps1
@@ -1,3 +1,5 @@
+$ErrorActionPreference = "Stop"
+
 function DownloadLanguageFile($languageFile, $innoPath) {
   $url = 'https://raw.github.com/jrsoftware/issrc/master/Files/Languages/Unofficial/' + $languageFile
   $installPath = $innoPath + '\Languages\' + $languageFile
@@ -10,7 +12,11 @@ $innoInstallPath = 'C:\Program Files (x86)\Inno Setup 5'
 # Install the Inno Download Plugin
 $url = 'https://bitbucket.org/mitrich_k/inno-download-plugin/downloads/idpsetup-1.5.1.exe'
 (New-Object System.Net.WebClient).DownloadFile($url, (pwd).path + '/idpsetup-1.5.1.exe')
+
 ./idpsetup-1.5.1.exe /verysilent
+if (!$?) {
+  throw 'Failed to install the Inno Download Plugin'
+}
 
 # Also install the unofficial Korean and Simplified Chinese translation
 # files for Inno Setup.
@@ -20,3 +26,6 @@ DownloadLanguageFile '\ChineseSimplified.isl'  $innoInstallPath
 $env:PATH += ';' + $innoInstallPath
 
 iscc scripts\installer.iss
+if ($LastExitCode -ne 0) {
+  throw 'Failed to build the LOOT installer'
+}
