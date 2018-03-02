b3c75dce68923b418414e7b6715339beb3f0f54d
Oliver Hamlet <oliver.hamlet@gmail.com>
2018-03-02 20:32:37 +0000

    Convert loot-menu element to ES2015 module

---
+++ src/gui/html/elements/loot-menu.js
@@ -0,0 +1,37 @@
+// import {PolymerElement} from '@polymer/polymer/polymer-element.js';
+// import {PaperListbox} from 'paper-listbox/paper-listbox.js">
+
+export class LootMenu extends Polymer.Element {
+  static get is() {
+    return 'loot-menu';
+  }
+
+  static get template() {
+    return Polymer.html`<style>
+        ::slotted(*) {
+          cursor: pointer;
+        }
+      </style>
+      <paper-listbox id="menu" multi>
+        <slot></slot>
+      </paper-listbox>`;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.$.menu.addEventListener('iron-select', LootMenu.onSelect);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+
+    this.$.menu.removeEventListener('iron-select', LootMenu.onSelect);
+  }
+
+  static onSelect(evt) {
+    evt.currentTarget.select(evt.currentTarget.indexOf(evt.detail.item));
+  }
+}
+
+customElements.define(LootMenu.is, LootMenu);
