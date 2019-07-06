70803938dddd82d6a4375ba96948dc12b19778d4
Oliver Hamlet <oliver.hamlet@gmail.com>
2019-07-06 14:02:59 +0100

    Reformat json.h

--- src/gui/cef/query/json.h
+++ src/gui/cef/query/json.h
@@ -28,20 +28,19 @@ along with LOOT.  If not, see
 #undef min
 #undef max
 
-#include <json.hpp>
 #include <loot/api.h>
+#include <json.hpp>
 
 #include "gui/cef/query/derived_plugin_metadata.h"
 
 namespace loot {
 void testConditionSyntax(const std::string& objectType,
-  const std::string& condition) {
+                         const std::string& condition) {
   try {
     ConditionalMetadata(condition).ParseCondition();
-  }
-  catch (std::exception& e) {
+  } catch (std::exception& e) {
     throw std::runtime_error(objectType +
-      " object has an invalid condition: " + e.what());
+                             " object has an invalid condition: " + e.what());
   }
 }
 
@@ -54,8 +53,8 @@ void validateMessageContents(const std::vector<MessageContent>& contents) {
     }
     if (!found)
       throw std::runtime_error(
-        "MessageContent array does not contain an English MessageContent "
-        "object");
+          "MessageContent array does not contain an English MessageContent "
+          "object");
   }
 }
 
@@ -102,25 +101,23 @@ void to_json(nlohmann::json& json, const MessageType& type) {
 
 void to_json(nlohmann::json& json, const SimpleMessage& message) {
   json = {
-    { "type", message.type },
-    { "text", message.text },
-    { "language", message.language },
-    { "condition", message.condition },
+      {"type", message.type},
+      {"text", message.text},
+      {"language", message.language},
+      {"condition", message.condition},
   };
 }
 
 void from_json(const nlohmann::json& json, Message& message) {
   if (json.count("text") == 0) {
-    throw std::runtime_error(
-      "SimpleMessage object has an empty 'text' value");
+    throw std::runtime_error("SimpleMessage object has an empty 'text' value");
   }
   if (json.count("language") == 0) {
     throw std::runtime_error(
-      "SimpleMessage object has an empty 'language' value");
+        "SimpleMessage object has an empty 'language' value");
   }
   if (json.count("type") == 0) {
-    throw std::runtime_error(
-      "SimpleMessage object has an empty 'type' value");
+    throw std::runtime_error("SimpleMessage object has an empty 'type' value");
   }
 
   auto condition = json.value("condition", "");
@@ -135,9 +132,9 @@ void from_json(const nlohmann::json& json, Message& message) {
 
 void to_json(nlohmann::json& json, const Tag& tag) {
   json = {
-    { "name", tag.GetName() },
-    { "condition", tag.GetCondition() },
-    { "isAddition", tag.IsAddition() },
+      {"name", tag.GetName()},
+      {"condition", tag.GetCondition()},
+      {"isAddition", tag.IsAddition()},
   };
 }
 
@@ -150,50 +147,45 @@ void from_json(const nlohmann::json& json, Tag& tag) {
 
   testConditionSyntax("Tag", condition);
 
-  tag = Tag(json.at("name"),
-    json.value("isAddition", false),
-    condition);
+  tag = Tag(json.at("name"), json.value("isAddition", false), condition);
 }
 
 void to_json(nlohmann::json& json, const MessageContent& content) {
   json = {
-    { "text", content.GetText() },
-    { "language", content.GetLanguage() },
+      {"text", content.GetText()},
+      {"language", content.GetLanguage()},
   };
 }
 
 void from_json(const nlohmann::json& json, MessageContent& content) {
-      if (json.count("text") == 0) {
-        throw std::runtime_error(
-            "MessageContent object has an empty 'text' value");
-      }
-      if (json.count("language") == 0) {
-        throw std::runtime_error(
-            "MessageContent object has an empty 'language' value");
-      }
+  if (json.count("text") == 0) {
+    throw std::runtime_error("MessageContent object has an empty 'text' value");
+  }
+  if (json.count("language") == 0) {
+    throw std::runtime_error(
+        "MessageContent object has an empty 'language' value");
+  }
 
-      content = MessageContent(json.at("text"), json.at("language"));
+  content = MessageContent(json.at("text"), json.at("language"));
 }
 
 void to_json(nlohmann::json& json, const PluginCleaningData& data) {
   json = {
-    { "crc", data.GetCRC() },
-    { "util", data.GetCleaningUtility() },
-    { "itm", data.GetITMCount() },
-    { "udr", data.GetDeletedReferenceCount() },
-    { "nav", data.GetDeletedNavmeshCount() },
-    { "info", data.GetInfo() },
+      {"crc", data.GetCRC()},
+      {"util", data.GetCleaningUtility()},
+      {"itm", data.GetITMCount()},
+      {"udr", data.GetDeletedReferenceCount()},
+      {"nav", data.GetDeletedNavmeshCount()},
+      {"info", data.GetInfo()},
   };
 }
 
 void from_json(const nlohmann::json& json, PluginCleaningData& data) {
   if (json.value("crc", 0) == 0) {
-    throw std::runtime_error(
-        "CleaningData object has a 'crc' value of zero");
+    throw std::runtime_error("CleaningData object has a 'crc' value of zero");
   }
   if (json.count("util") == 0) {
-    throw std::runtime_error(
-        "CleaningData object has an empty 'util' value");
+    throw std::runtime_error("CleaningData object has an empty 'util' value");
   }
 
   auto info = json.value("info", std::vector<MessageContent>());
@@ -210,9 +202,9 @@ void from_json(const nlohmann::json& json, PluginCleaningData& data) {
 
 void to_json(nlohmann::json& json, const File& file) {
   json = {
-    { "name", file.GetName() },
-    { "display", file.GetDisplayName() },
-    { "condition", file.GetCondition() },
+      {"name", file.GetName()},
+      {"display", file.GetDisplayName()},
+      {"condition", file.GetCondition()},
   };
 }
 
@@ -225,15 +217,13 @@ void from_json(const nlohmann::json& json, File& file) {
 
   testConditionSyntax("File", condition);
 
-  file = File(json.at("name"),
-    json.value("display", ""),
-    condition);
+  file = File(json.at("name"), json.value("display", ""), condition);
 }
 
 void to_json(nlohmann::json& json, const Group& group) {
   json = {
-    { "name", group.GetName() },
-    { "after", group.GetAfterGroups() },
+      {"name", group.GetName()},
+      {"after", group.GetAfterGroups()},
   };
 }
 
@@ -247,13 +237,13 @@ void from_json(const nlohmann::json& json, Group& group) {
   testConditionSyntax("File", condition);
 
   group = Group(json.at("name"),
-    json.value("after", std::unordered_set<std::string>()));
+                json.value("after", std::unordered_set<std::string>()));
 }
 
 void to_json(nlohmann::json& json, const Location& location) {
   json = {
-    { "link", location.GetURL() },
-    { "name", location.GetName() },
+      {"link", location.GetURL()},
+      {"name", location.GetName()},
   };
 }
 
@@ -267,22 +257,22 @@ void from_json(const nlohmann::json& json, Location& location) {
 
 void to_json(nlohmann::json& json, const MasterlistInfo& info) {
   json = {
-    { "revision", info.revision_id },
-    { "date", info.revision_date },
+      {"revision", info.revision_id},
+      {"date", info.revision_date},
   };
 }
 
 void to_json(nlohmann::json& json, const GameSettings& game) {
   json = {
-    { "type", GameSettings(game.Type()).FolderName() },
-    { "name", game.Name() },
-    { "master", game.Master() },
-    { "registry", game.RegistryKey() },
-    { "folder", game.FolderName() },
-    { "repo", game.RepoURL() },
-    { "branch", game.RepoBranch() },
-    { "path", game.GamePath().u8string() },
-    { "localPath", game.GameLocalPath().u8string() },
+      {"type", GameSettings(game.Type()).FolderName()},
+      {"name", game.Name()},
+      {"master", game.Master()},
+      {"registry", game.RegistryKey()},
+      {"folder", game.FolderName()},
+      {"repo", game.RepoURL()},
+      {"branch", game.RepoBranch()},
+      {"path", game.GamePath().u8string()},
+      {"localPath", game.GameLocalPath().u8string()},
   };
 }
 
@@ -303,18 +293,16 @@ void from_json(const nlohmann::json& json, GameSettings& game) {
 
 nlohmann::json to_json_with_language(const PluginMetadata& metadata,
                                      const std::string& language) {
-  nlohmann::json json = {
-    { "name", metadata.GetName() },
-    { "enabled", metadata.IsEnabled() },
-    { "after", metadata.GetLoadAfterFiles() },
-    { "req", metadata.GetRequirements() },
-    { "inc", metadata.GetIncompatibilities() },
-    { "msg", metadata.GetSimpleMessages(language) },
-    { "tag", metadata.GetTags() },
-    { "dirty", metadata.GetDirtyInfo() },
-    { "clean", metadata.GetCleanInfo() },
-    { "url", metadata.GetLocations() }
-  };
+  nlohmann::json json = {{"name", metadata.GetName()},
+                         {"enabled", metadata.IsEnabled()},
+                         {"after", metadata.GetLoadAfterFiles()},
+                         {"req", metadata.GetRequirements()},
+                         {"inc", metadata.GetIncompatibilities()},
+                         {"msg", metadata.GetSimpleMessages(language)},
+                         {"tag", metadata.GetTags()},
+                         {"dirty", metadata.GetDirtyInfo()},
+                         {"clean", metadata.GetCleanInfo()},
+                         {"url", metadata.GetLocations()}};
 
   if (metadata.GetGroup().has_value()) {
     json["group"] = metadata.GetGroup().value();
@@ -325,8 +313,7 @@ nlohmann::json to_json_with_language(const PluginMetadata& metadata,
 
 void from_json(const nlohmann::json& json, PluginMetadata& metadata) {
   if (json.count("name") == 0) {
-    throw std::runtime_error(
-      "PluginMetadata object has an empty 'name' value");
+    throw std::runtime_error("PluginMetadata object has an empty 'name' value");
   }
 
   metadata = PluginMetadata(json.at("name").get<std::string>());
@@ -334,24 +321,23 @@ void from_json(const nlohmann::json& json, PluginMetadata& metadata) {
   if (metadata.IsRegexPlugin()) {
     try {
       std::regex(metadata.GetName(),
-        std::regex::ECMAScript | std::regex::icase);
-    }
-    catch (std::regex_error& e) {
+                 std::regex::ECMAScript | std::regex::icase);
+    } catch (std::regex_error& e) {
       throw std::runtime_error(
-        std::string(
-          "PluginMetadata object has an invalid regex 'name' value: ") +
-        e.what());
+          std::string(
+              "PluginMetadata object has an invalid regex 'name' value: ") +
+          e.what());
     }
 
     if (json.count("dirty") != 0) {
       throw std::runtime_error(
-        "PluginMetadata object cannot have a 'dirty' key with a regex "
-        "'name' value");
+          "PluginMetadata object cannot have a 'dirty' key with a regex "
+          "'name' value");
     }
     if (json.count("clean") != 0) {
       throw std::runtime_error(
-        "PluginMetadata object cannot have a 'clean' key with a regex "
-        "'name' value");
+          "PluginMetadata object cannot have a 'clean' key with a regex "
+          "'name' value");
     }
   }
 
@@ -375,16 +361,16 @@ void from_json(const nlohmann::json& json, PluginMetadata& metadata) {
 template<typename G>
 void to_json(nlohmann::json& json, const DerivedPluginMetadata<G>& plugin) {
   json = {
-    { "name", plugin.name },
-    { "isActive", plugin.isActive },
-    { "isDirty", plugin.isDirty },
-    { "isEmpty", plugin.isEmpty },
-    { "isMaster", plugin.isMaster },
-    { "isLightMaster", plugin.isLightMaster },
-    { "loadsArchive", plugin.loadsArchive },
-    { "messages", plugin.messages },
-    { "suggestedTags", plugin.suggestedTags },
-    { "currentTags", plugin.currentTags },
+      {"name", plugin.name},
+      {"isActive", plugin.isActive},
+      {"isDirty", plugin.isDirty},
+      {"isEmpty", plugin.isEmpty},
+      {"isMaster", plugin.isMaster},
+      {"isLightMaster", plugin.isLightMaster},
+      {"loadsArchive", plugin.loadsArchive},
+      {"messages", plugin.messages},
+      {"suggestedTags", plugin.suggestedTags},
+      {"currentTags", plugin.currentTags},
   };
 
   if (plugin.version.has_value()) {
@@ -408,13 +394,14 @@ void to_json(nlohmann::json& json, const DerivedPluginMetadata<G>& plugin) {
   }
 
   if (plugin.masterlistMetadata.has_value()) {
-    json["masterlist"] = to_json_with_language(plugin.masterlistMetadata.value(), plugin.language);
+    json["masterlist"] = to_json_with_language(
+        plugin.masterlistMetadata.value(), plugin.language);
   }
 
   if (plugin.userMetadata.has_value()) {
-    json["userlist"] = to_json_with_language(plugin.userMetadata.value(), plugin.language);
+    json["userlist"] =
+        to_json_with_language(plugin.userMetadata.value(), plugin.language);
   }
-}
 }
 
 #endif
