829257a4ec342089edc80f1dab449bfa897c3b34
sibir <ine.sibir@gmail.com>
2021-04-21 12:26:43 -0700

    Add comments for translators to a few strings (#1489)
    Add add-comments argument to pre-commit hook.
    
    Add notes & define some jargon for a few strings.

--- src/gui/state/loot_state.cpp
+++ src/gui/state/loot_state.cpp
@@ -101,8 +101,10 @@ LootState::LootState(const std::filesystem::path& lootAppPath,
 
 void LootState::init(const std::string& cmdLineGame, bool autoSort) {
   if (autoSort && cmdLineGame.empty()) {
-    initErrors_.push_back(translate(
-        "Error: --auto-sort was passed but no --game parameter was provided."));
+    initErrors_.push_back(
+      /* translators: --auto-sort and --game are command-line arguments and shouldn't be translated. */
+      translate("Error: --auto-sort was passed but no --game parameter was "
+                "provided."));
   } else {
     setAutoSort(autoSort);
   }
