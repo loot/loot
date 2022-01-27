5308131eae20aa682f07722ca7a6ed9c4ac79148
Oliver Hamlet <oliver@ortham.io>
2022-01-27 17:45:15 +0000

    Fix missing translator comment

--- src/gui/state/game/game.cpp
+++ src/gui/state/game/game.cpp
@@ -363,13 +363,11 @@ std::vector<Message> Game::CheckInstallValidity(
     }
     messages.push_back(PlainTextMessage(
         MessageType::warn,
-        (boost::format(
-             /* translators: A header is the part of a file that stores data like file name and version. */
-             boost::locale::translate(
-                 "This plugin has a header version of %1%, "
-                 "which is less than the game's minimum supported header "
-                 "version of "
-                 "%2%.")) %
+        (boost::format(boost::locale::translate(
+             /* translators: A header is the part of a file that stores data
+                like file name and version. */
+             "This plugin has a header version of %1%, which is less than "
+             "the game's minimum supported header version of %2%.")) %
          plugin->GetHeaderVersion().value() % MinimumHeaderVersion())
             .str()));
   }
