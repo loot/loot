loot/loot

commit a0affbe740e5e960efcc910e870532916e118568

Add support for Oblivion Remastered

src/gui/qt/settings/game_tab.cpp
@@ -79,7 +79,7 @@ const std::map<std::string, GameId> GameTab::GAME_IDS_BY_STRING({
     {ToString(GameId::fo4), GameId::fo4},
     {ToString(GameId::fo4vr), GameId::fo4vr},
     {ToString(GameId::starfield), GameId::starfield},
-    {ToString(GameId::openmw), GameId::openmw},
+    {ToString(GameId::oblivionRemastered), GameId::oblivionRemastered},
 });
 
 GameTab::GameTab(const GameSettings& settings,

resources/linux/io.github.loot.loot.metainfo.xml
@@ -28,6 +28,7 @@
       <li>Starfield</li>
       <li>The Elder Scrolls III: Morrowind</li>
       <li>The Elder Scrolls IV: Oblivion</li>
+      <li>The Elder Scrolls IV: Oblivion Remastered</li>
       <li>The Elder Scrolls V: Skyrim</li>
       <li>The Elder Scrolls V: Skyrim Special Edition (and Anniversary Edition)</li>
       <li>The Elder Scrolls V: Skyrim VR</li>

scripts/installer.iss
@@ -171,6 +171,10 @@ Source: "build\masterlists\Oblivion\masterlist.yaml"; \
 DestDir: "{localappdata}\{#MyAppName}\games\Oblivion"; Flags: ignoreversion
 Source: "build\masterlists\Oblivion\masterlist.yaml.metadata.toml"; \
 DestDir: "{localappdata}\{#MyAppName}\games\Oblivion"; Flags: ignoreversion
+Source: "build\masterlists\Oblivion\masterlist.yaml"; \
+DestDir: "{localappdata}\{#MyAppName}\games\Oblivion Remastered"; Flags: ignoreversion
+Source: "build\masterlists\Oblivion\masterlist.yaml.metadata.toml"; \
+DestDir: "{localappdata}\{#MyAppName}\games\Oblivion Remastered"; Flags: ignoreversion
 Source: "build\masterlists\Nehrim\masterlist.yaml"; \
 DestDir: "{localappdata}\{#MyAppName}\games\Nehrim"; Flags: ignoreversion
 Source: "build\masterlists\Nehrim\masterlist.yaml.metadata.toml"; \
@@ -525,6 +529,7 @@ begin
     DownloadPage.Add('https://raw.githubusercontent.com/loot/prelude/{#MasterlistBranch}/prelude.yaml', 'prelude\prelude.yaml', '');
     DownloadPage.Add('https://raw.githubusercontent.com/loot/morrowind/{#MasterlistBranch}/masterlist.yaml', 'games\Morrowind\masterlist.yaml', '');
     DownloadPage.Add('https://raw.githubusercontent.com/loot/oblivion/{#MasterlistBranch}/masterlist.yaml', 'games\Oblivion\masterlist.yaml', '');
+    DownloadPage.Add('https://raw.githubusercontent.com/loot/oblivion/{#MasterlistBranch}/masterlist.yaml', 'games\Oblivion Remastered\masterlist.yaml', '');
     DownloadPage.Add('https://raw.githubusercontent.com/loot/oblivion/{#MasterlistBranch}/masterlist.yaml', 'games\Nehrim\masterlist.yaml', '');
     DownloadPage.Add('https://raw.githubusercontent.com/loot/skyrim/{#MasterlistBranch}/masterlist.yaml', 'games\Skyrim\masterlist.yaml', '');
     DownloadPage.Add('https://raw.githubusercontent.com/loot/enderal/{#MasterlistBranch}/masterlist.yaml', 'games\Enderal\masterlist.yaml', '');
@@ -567,6 +572,7 @@ begin
       InstallMetadataFile('prelude\prelude.yaml');
       InstallMetadataFile('games\Morrowind\masterlist.yaml');
       InstallMetadataFile('games\Oblivion\masterlist.yaml');
+      InstallMetadataFile('games\Oblivion Remastered\masterlist.yaml');
       InstallMetadataFile('games\Nehrim\masterlist.yaml');
       InstallMetadataFile('games\Skyrim\masterlist.yaml');
       InstallMetadataFile('games\Enderal\masterlist.yaml');

src/gui/state/game/detection/common.cpp
@@ -67,6 +67,11 @@ std::string GetExecutableName(GameId gameId) {
 #else
       return "openmw";
 #endif
+    case GameId::oblivionRemastered:
+      // This is the Steam version's executable, for the Game Pass version it's
+      // OblivionRemastered/Binaries/WinGDK/OblivionRemastered-WinGDK-Shipping.exe
+      return "OblivionRemastered/Binaries/Win64/"
+             "OblivionRemastered-Win64-Shipping.exe";
     default:
       throw std::logic_error("Unrecognised game ID");
   }
@@ -96,6 +101,7 @@ bool ExecutableExists(const GameId& gameType,
     case GameId::fo3:
     case GameId::fonv:
     case GameId::starfield:
+    case GameId::oblivionRemastered:
       // Don't bother checking for the games that don't share their master
       // plugin name.
       return true;

src/gui/state/game/detection/common.h
@@ -36,7 +36,7 @@
 #include "gui/state/game/game_id.h"
 
 namespace loot {
-static const std::array<GameId, 14> ALL_GAME_IDS = {GameId::tes3,
+static const std::array<GameId, 15> ALL_GAME_IDS = {GameId::tes3,
                                                     GameId::tes4,
                                                     GameId::nehrim,
                                                     GameId::tes5,
@@ -49,7 +49,8 @@ static const std::array<GameId, 14> ALL_GAME_IDS = {GameId::tes3,
                                                     GameId::fo4,
                                                     GameId::fo4vr,
                                                     GameId::starfield,
-                                                    GameId::openmw};
+                                                    GameId::openmw,
+                                                    GameId::oblivionRemastered};
 
 struct LocalisedGameInstallPath {
   std::filesystem::path installPath;

src/gui/state/game/detection/epic_games_store.cpp
@@ -82,6 +82,7 @@ std::optional<std::string> GetEgsAppName(GameId gameId) {
     case GameId::fo4vr:
     case GameId::starfield:
     case GameId::openmw:
+    case GameId::oblivionRemastered:
       return std::nullopt;
     default:
       throw std::logic_error("Unrecognised game type");

src/gui/state/game/detection/generic.cpp
@@ -96,6 +96,7 @@ std::vector<RegistryValue> GetRegistryValues(const GameId gameId) {
                             "Software\\Bethesda Softworks\\Fallout 4 VR",
                             "Installed Path"}};
     case GameId::starfield:
+    case GameId::oblivionRemastered:
       return {};
     case GameId::openmw:
       return {RegistryValue{RegistryRootKey::LOCAL_MACHINE,
@@ -134,6 +135,10 @@ bool IsSteamInstall(const GameId gameId,
       return std::filesystem::exists(installPath / "steam_api64.dll");
     case GameId::openmw:
       return false;
+    case GameId::oblivionRemastered:
+      return std::filesystem::exists(installPath / "Engine" / "Binaries" /
+                                     "ThirdParty" / "Steamworks" / "Steamv153" /
+                                     "Win64" / "steam_api64.dll");
     default:
       throw std::logic_error("Unrecognised game ID");
   }
@@ -183,6 +188,7 @@ bool IsMicrosoftInstall(const GameId gameId,
     case GameId::tes5se:
     case GameId::fo4:
     case GameId::starfield:
+    case GameId::oblivionRemastered:
       return std::filesystem::exists(installPath / "appxmanifest.xml");
     default:
       return false;

src/gui/state/game/detection/microsoft_store.cpp
@@ -62,6 +62,7 @@ std::vector<loot::LocalisedGameInstallPath> GetGameLocalisationDirectories(
     case GameId::tes5se:
     case GameId::fo4:
     case GameId::starfield:
+    case GameId::oblivionRemastered:
       // There's only one path, it could be for any language that
       // the game supports, so just use en (the choice doesn't
       // matter).
@@ -80,6 +81,7 @@ bool IsOnMicrosoftStore(const GameId gameId) {
     case GameId::fonv:
     case GameId::fo4:
     case GameId::starfield:
+    case GameId::oblivionRemastered:
       return true;
     case GameId::nehrim:
     case GameId::enderal:
@@ -117,6 +119,9 @@ std::filesystem::path GetGameContentPath(
       return xboxGamingRootPath / "Fallout 4 (PC)" / "Content";
     case GameId::starfield:
       return xboxGamingRootPath / "Starfield" / "Content";
+    case GameId::oblivionRemastered:
+      return xboxGamingRootPath / "The Elder Scrolls IV- Oblivion Remastered" /
+             "Content";
     default:
       throw std::logic_error("Unsupported Microsoft Store game");
   }

commit ef076de531c74aa4a6c7f765717065fa23e9f572

Fix saving settings with OpenMW installed
I accidentally broke this in a0affbe740e5e960efcc910e870532916e118568.

resources/l10n/template.pot
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: LOOT 0.26.0\n"
 "Report-Msgid-Bugs-To: https://github.com/loot/loot/issues\n"
-"POT-Creation-Date: 2025-05-01 22:43+0100\n"
+"POT-Creation-Date: 2025-05-03 09:57+0100\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
 "Language-Team: LANGUAGE <LL@li.org>\n"
@@ -939,7 +939,7 @@ msgid "URL"
 msgstr ""
 
 #: src/gui/qt/plugin_editor/models/location_table_model.cpp:59
-#: src/gui/qt/settings/game_tab.cpp:150
+#: src/gui/qt/settings/game_tab.cpp:151
 #: src/gui/qt/settings/new_game_dialog.cpp:123
 msgid "Name"
 msgstr ""
@@ -973,37 +973,37 @@ msgstr ""
 msgid "Browse…"
 msgstr ""
 
-#: src/gui/qt/settings/game_tab.cpp:151
+#: src/gui/qt/settings/game_tab.cpp:152
 #: src/gui/qt/settings/new_game_dialog.cpp:124
 msgid "Base Game"
 msgstr ""
 
-#: src/gui/qt/settings/game_tab.cpp:152
+#: src/gui/qt/settings/game_tab.cpp:153
 #: src/gui/qt/settings/new_game_dialog.cpp:125
 msgid "LOOT Folder"
 msgstr ""
 
-#: src/gui/qt/settings/game_tab.cpp:153
+#: src/gui/qt/settings/game_tab.cpp:154
 msgid "Main Master Plugin"
 msgstr ""
 
-#: src/gui/qt/settings/game_tab.cpp:154
+#: src/gui/qt/settings/game_tab.cpp:155
 msgid "Minimum Header Version"
 msgstr ""
 
-#: src/gui/qt/settings/game_tab.cpp:155
+#: src/gui/qt/settings/game_tab.cpp:156
 msgid "Masterlist Source"
 msgstr ""
 
-#: src/gui/qt/settings/game_tab.cpp:156
+#: src/gui/qt/settings/game_tab.cpp:157
 msgid "Install Path"
 msgstr ""
 
-#: src/gui/qt/settings/game_tab.cpp:157
+#: src/gui/qt/settings/game_tab.cpp:158
 msgid "Local AppData Path"
 msgstr ""
 
-#: src/gui/qt/settings/game_tab.cpp:159
+#: src/gui/qt/settings/game_tab.cpp:160
 msgid "Delete game"
 msgstr ""
 

src/gui/qt/settings/game_tab.cpp
@@ -79,6 +79,7 @@ const std::map<std::string, GameId> GameTab::GAME_IDS_BY_STRING({
     {ToString(GameId::fo4), GameId::fo4},
     {ToString(GameId::fo4vr), GameId::fo4vr},
     {ToString(GameId::starfield), GameId::starfield},
+    {ToString(GameId::openmw), GameId::openmw},
     {ToString(GameId::oblivionRemastered), GameId::oblivionRemastered},
 });
 