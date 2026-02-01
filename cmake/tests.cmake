651a9518f8bcc64548818757ef8f268c0f47552c
Oliver Hamlet <git@ortham.net>
2026-02-01 19:56:37 +0000

    Fix building only the tests for Windows

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -217,13 +217,13 @@ if(WIN32)
         add_custom_command(TARGET loot_gui_tests POST_BUILD
             COMMAND ${QT_DIR}/bin/windeployqt $<TARGET_FILE:loot_gui_tests>
             COMMENT "Running windeployqt...")
-
-        # Copy the API binary to the build directory.
-        add_custom_command(TARGET loot_gui_tests POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                $<TARGET_FILE:libloot::libloot>
-                "$<TARGET_FILE_DIR:loot_gui_tests>/$<PATH:GET_FILENAME,$<TARGET_FILE:libloot::libloot>>")
     endif()
+
+    # Copy the API binary to the build directory.
+    add_custom_command(TARGET loot_gui_tests POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            $<TARGET_FILE:libloot::libloot>
+            "$<TARGET_FILE_DIR:loot_gui_tests>/$<TARGET_FILE_NAME:libloot::libloot>")
 endif()
 
 # Copy testing plugins
