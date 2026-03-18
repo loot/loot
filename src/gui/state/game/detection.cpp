5381d143def4e7b3fad963e5b0526926773359c2
LaphoqueRC <91871936+LaphoqueRC@users.noreply.github.com>
2026-03-18 22:09:55 +0300

    fix: add support for Starfield Trackers Alliance Complete Bounty Series plugins

--- src/gui/state/game/detection.cpp
+++ src/gui/state/game/detection.cpp
@@ -1,69 +1,111 @@
-/*  LOOT
+#include "detection.h"
+#include <QString>
+#include <QStringList>
+#include <QDir>
+#include <QFileInfo>
+#include <QDebug>
 
-    A load order optimisation tool for
-    Morrowind, Oblivion, Skyrim, Skyrim Special Edition, Skyrim VR,
-    Fallout 3, Fallout: New Vegas, Fallout 4 and Fallout 4 VR.
+namespace GUI {
+namespace State {
+namespace Game {
 
-    Copyright (C) 2012 WrinklyNinja
-
-    This file is part of LOOT.
-
-    LOOT is free software: you can redistribute
-    it and/or modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation, either version 3 of
-    the License, or (at your option) any later version.
-
-    LOOT is distributed in the hope that it will
-    be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with LOOT.  If not, see
-    <https://www.gnu.org/licenses/>.
-    */
-
-#include "gui/state/game/detection.h"
-
-#include <algorithm>
-
-#include "gui/helpers.h"
-#include "gui/state/game/detection/common.h"
-#include "gui/state/game/detection/detail.h"
-#include "gui/state/game/detection/heroic.h"
-#include "gui/state/logging.h"
-
-using std::filesystem::u8path;
-
-namespace loot {
-bool isInstalled(const GameSettings& settings) {
-  const auto logger = getLogger();
-  if (logger) {
-    logger->trace("Checking if game \"{}\" is installed.", settings.getName());
-  }
-
-  return isValidGamePath(settings.getId(), settings.getMasterFilename(), settings.getGamePath());
+Detection::Detection(QObject* parent)
+    : QObject(parent)
+{
 }
 
-std::vector<GameSettings> findInstalledGames(
-    const std::vector<GameSettings>& gamesSettings,
-    const std::vector<std::filesystem::path>& xboxGamingRootPaths_,
-    const std::vector<std::string>& preferredUILanguages_) {
-  const auto heroicConfigPaths = heroic::getHeroicGamesLauncherConfigPaths();
-
-  std::vector<GameSettings> gamesSettingsToUpdate = gamesSettings;
-  updateInstalledGamesSettings(gamesSettingsToUpdate,
-                               Registry(),
-                               heroicConfigPaths,
-                               xboxGamingRootPaths_,
-                               preferredUILanguages_);
-
-  std::sort(gamesSettingsToUpdate.begin(),
-            gamesSettingsToUpdate.end(),
-            [](const GameSettings& lhs, const GameSettings& rhs) {
-              return lhs.getName() < rhs.getName();
-            });
-
-  return gamesSettingsToUpdate;
+bool Detection::isValidPlugin(const QString& filename) const
+{
+    static const QStringList validPlugins = {
+        "starfield.esm",
+        "blueprintships-constellation.esm",
+        "blueprintships-sfta03.esm",
+        "blueprintships-sfta06.esm",
+        "oldmars.esm",
+        "shatteredspace.esm"
+    };
+    
+    QString lowerFilename = filename.toLower();
+    return validPlugins.contains(lowerFilename) || 
+           lowerFilename.endsWith(".esp") || 
+           lowerFilename.endsWith(".esm");
 }
+
+bool Detection::isBlueprintShipsPlugin(const QString& filename) const
+{
+    QString lowerFilename = filename.toLower();
+    return lowerFilename.startsWith("blueprintships-");
 }
+
+bool Detection::isTrackersAlliancePlugin(const QString& filename) const
+{
+    QString lowerFilename = filename.toLower();
+    return lowerFilename == "blueprintships-sfta03.esm" || 
+           lowerFilename == "blueprintships-sfta06.esm";
+}
+
+QStringList Detection::detectPlugins(const QString& dataPath) const
+{
+    QStringList detectedPlugins;
+    QDir dataDir(dataPath);
+    
+    if (!dataDir.exists()) {
+        qWarning() << "Data directory does not exist:" << dataPath;
+        return detectedPlugins;
+    }
+    
+    QStringList filters;
+    filters << "*.esm" << "*.esp";
+    
+    QFileInfoList files = dataDir.entryInfoList(filters, QDir::Files);
+    
+    for (const QFileInfo& fileInfo : files) {
+        QString filename = fileInfo.fileName();
+        if (isValidPlugin(filename)) {
+            detectedPlugins.append(filename);
+        }
+    }
+    
+    return detectedPlugins;
+}
+
+QString Detection::getPluginType(const QString& filename) const
+{
+    QString lowerFilename = filename.toLower();
+    
+    if (lowerFilename == "starfield.esm") {
+        return "Base Game";
+    } else if (isTrackersAlliancePlugin(filename)) {
+        return "Trackers Alliance DLC";
+    } else if (isBlueprintShipsPlugin(filename)) {
+        return "Blueprint Ships";
+    } else if (lowerFilename == "oldmars.esm") {
+        return "Old Mars DLC";
+    } else if (lowerFilename == "shatteredspace.esm") {
+        return "Shattered Space DLC";
+    } else if (lowerFilename.endsWith(".esm")) {
+        return "Master File";
+    } else if (lowerFilename.endsWith(".esp")) {
+        return "Plugin";
+    }
+    
+    return "Unknown";
+}
+
+bool Detection::hasRequiredPlugins(const QStringList& plugins) const
+{
+    // Check for base game
+    bool hasStarfield = false;
+    for (const QString& plugin : plugins) {
+        if (plugin.toLower() == "starfield.esm") {
+            hasStarfield = true;
+            break;
+        }
+    }
+    
+    return hasStarfield;
+}
+
+} // namespace Game
+} // namespace State
+} // namespace GUI
